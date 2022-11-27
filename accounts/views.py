8d9785ac248e34d591d5ab1644be0e93708bbc23
choki-dorji <62417033+choki-dorji@users.noreply.github.com>
2022-11-27 11:55:06 +0600

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -524,21 +524,7 @@ def personal(request):
 
 
 
-@login_required(login_url='login')
 def home(request):
-	orders = Order.objects.all()
-	customers = Customer.objects.all()
-
-	total_customers = customers.count()
-
-	total_orders = orders.count()
-	delivered = orders.filter(status='Delivered').count()
-	pending = orders.filter(status='Pending').count()
-
-	context = {'orders':orders, 'customers':customers,
-	'total_orders':total_orders,'delivered':delivered,
-	'pending':pending }
-
 	return render(request, 'accounts/dashboard.html', context)
 
 def delete(request):
